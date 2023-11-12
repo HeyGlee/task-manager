d85fd886800509369632cf93aa6bba6eb24a09f4
Alex Scripcariu <alexscripariu412@gmail.com>
2023-11-12 07:30:16 +0000

    added the main part of the menu, just need a lib import

---
+++ main.py
@@ -0,0 +1,46 @@
+from time import sleep
+import
+
+
+def main():
+    options = ["Add a task",
+               "Delete a task",
+               "Search tasks",
+               "Update a task",
+               "Save tasks"]
+
+    print("TASK MANAGER")
+    print("-" * 50)
+    sleep(0.8)
+    print("Please select an option: ")
+    sleep(0.8)
+    for i, v in enumerate(options):
+        print(f"{i + 1}) {v}")
+    sleep(0.5)
+
+    def get_method() -> int:
+        while True:
+            try:
+                selected = int(input("Please select an option (number): "))
+            except ValueError:
+                print("ERROR: please type in a number!")
+                continue
+
+            if 1 > selected > len(options):
+                print(f"Please enter a number between 1-{len(options)}")
+                continue
+
+            return selected
+
+    method = get_method()
+
+    match method:
+        case 1:
+            pass
+
+
+
+
+
+if __name__ == "__main__":
+    main()
